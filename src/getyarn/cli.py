d319ce7469f67b27ab4668a7904a5b22f16f3949
gaardhus <tobias_gaardhus@hotmail.com>
2024-03-20 19:24:18 +0100

    CLI ready

---
+++ src/getyarn/cli.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+import argparse
+
+from moviepy.editor import concatenate_videoclips
+
+from .getyarn import get_yarn
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="Yarn URL")
+    parser.add_argument("output", type=str, help="Output file")
+    parser.add_argument(
+        "--next-clips", type=int, default=0, help="Number of next clips to download"
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    main_clip, next_clip = get_yarn(args.url)
+
+    clips = [main_clip]
+
+    for _ in range(args.next_clips):
+        sub_clip, next_clip = get_yarn(next_clip)
+        clips.append(sub_clip)
+
+    merged_clip = concatenate_videoclips(clips)
+
+    merged_clip.write_videofile(args.output)
+
+
+if __name__ == "__main__":
+    main()
--- src/getyarn/getyarn.py
+++ src/getyarn/getyarn.py
@@ -1,11 +1,18 @@
 from __future__ import annotations
 
+import tempfile
+
 import requests
 from bs4 import BeautifulSoup as bs
-from moviepy.editor import VideoFileClip, concatenate_videoclips
+from moviepy.editor import VideoFileClip
+
+# TODO: Replace moviepy with ffmpeg
+
+DOMAIN = "https://getyarn.io"
+TMP_DIR = tempfile.gettempdir()
 
 
-def get_yarn(url) -> tuple[bytes, str]:
+def get_yarn(url) -> tuple[VideoFileClip, str]:
     response = requests.get(url)
     soup = bs(response.content, "html.parser")
     source = soup.find("source").get("src")
@@ -17,24 +24,17 @@ def get_yarn(url) -> tuple[bytes, str]:
         raise ValueError(msg)
     mp4 = get_video_source(source)
 
-    next_clip = soup.find("a", title="Next Clip").get("href")
+    next_clip = DOMAIN + soup.find("a", title="Next Clip").get("href")
     if not next_clip:
         msg = "No next clip found"
         raise ValueError(msg)
 
-    print(next_clip)
-
     return mp4, next_clip
 
 
-def get_video_source(url) -> bytes:
+def get_video_source(url) -> VideoFileClip:
     response = requests.get(url)
     response.raise_for_status()
-    return response.content
-
-
-concatenate_videoclips([]).write_videofile("output.mp4")
-
-if __name__ == "__main__":
-    url = "https://getyarn.io/yarn-clip/4b6e7f8e-5e5e-4b3e-8f8e-7e4b5e5e3e8f"
-    get_yarn(url)
+    with tempfile.NamedTemporaryFile(suffix=".mp4", dir=TMP_DIR, delete=False) as f:
+        f.write(response.content)
+        return VideoFileClip(f.name)
