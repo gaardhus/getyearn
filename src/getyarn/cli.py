4fea08ca7a454b3115d33e9ae0012b104baf921f
gaardhus <tobias_gaardhus@hotmail.com>
2024-04-07 21:40:25 +0200

    Add default path

--- src/getyarn/cli.py
+++ src/getyarn/cli.py
@@ -1,6 +1,10 @@
+# TODO:
+# - Add clipping time range option
+
 from __future__ import annotations
 
 import argparse
+import os
 
 from moviepy.editor import concatenate_videoclips
 
@@ -8,29 +12,50 @@ from .getyarn import get_yarn
 
 
 def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="Get and merge video clips from https://getyarn.io/")
-    parser.add_argument("url", help="Yarn URL")
+    parser = argparse.ArgumentParser(
+        description="Get and merge video clips from https://getyarn.io/"
+    )
+    parser.add_argument("url", nargs="*", help="Yarn URLs")
     parser.add_argument("output", type=str, help="Output file")
     parser.add_argument(
-        "--next-clips", type=int, default=0, help="Number of next clips to download and merge"
+        "--output-dir",
+        type=str,
+        default="~/Videos/clips/",
+        help="Output directory",
     )
+    parser.add_argument(
+        "--next-clips",
+        type=int,
+        default=0,
+        help="Number of next clips to download and merge",
+    )
+    parser.add_argument(
+        "--method",
+        type=str,
+        choices=["chain", "compose"],
+        default="chain",
+        help="Method to merge clips",
+    )  # NOTE: Should the method be automatically determined based multiple URLs vs next-clips?
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
 
-    main_clip, next_clip = get_yarn(args.url)
+    clips = []
 
-    clips = [main_clip]
+    for url in args.url:
+        main_clip, next_clip = get_yarn(url)
+        clips.append(main_clip)
 
-    for _ in range(args.next_clips):
-        sub_clip, next_clip = get_yarn(next_clip)
-        clips.append(sub_clip)
+        for _ in range(args.next_clips):
+            sub_clip, next_clip = get_yarn(next_clip)
+            clips.append(sub_clip)
 
-    merged_clip = concatenate_videoclips(clips)
+    merged_clip = concatenate_videoclips(clips, method=args.method)
 
-    merged_clip.write_videofile(args.output)
+    out_file = os.path.expanduser(os.path.join(args.output_dir, args.output))
+    merged_clip.write_videofile(out_file)
 
 
 if __name__ == "__main__":
